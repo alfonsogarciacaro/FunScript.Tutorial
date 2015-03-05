cca96fa2e99312f875946ec5bd5a91ac613264ec
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2015-03-05 20:37:06 +0100

    Revert "Changes for London Talk"
    This reverts commit b736fda12ae0f38bccf5d16c922b38dd7e6edc4a.

--- tutorials/nodeserver.js
+++ tutorials/nodeserver.js
@@ -1,14 +1,13 @@
-var http = require('http');
-var finalhandler = require('finalhandler');
-var serveStatic = require('serve-static');
+var http = require('http')
+var finalhandler = require('finalhandler')
+var serveStatic = require('serve-static')
 
-var path = process.argv[2];
-var port = Number(process.argv[3]);
-port = isNaN(port) ? 80 : port;
+var port = Number(process.argv[2])
+port = isNaN(port) ? 8080 : port
 
-var serve = serveStatic("./" + path);
+var serve = serveStatic("./")
 var server = http.createServer(function(req, res){
-    var done = finalhandler(req, res);
-    serve(req, res, done);
-}).listen(port);
-console.log("Server directed to " + path + " running at localhost:" + port);
+    var done = finalhandler(req, res)
+    serve(req, res, done)
+}).listen(port)
+console.log("Server running at localhost:" + port)
